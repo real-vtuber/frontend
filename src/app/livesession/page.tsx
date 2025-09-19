d40a7c7a073e3c27fb999be9f9bccbd8914bc891
Zeddli <ruj8320@gmail.com>
2025-09-19 09:15:46 +0800

    page routing

---
+++ src/app/livesession/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LiveSession from '../../pages/LiveSession'
+
+const LiveSessionPage = () => {
+  return (
+    <LiveSessionPage/>
+  )
+}
+
+export default LiveSessionPage
