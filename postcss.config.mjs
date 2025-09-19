5c590ce705c06fc4d7170d5828ecea051d885052
Zeddli <ruj8320@gmail.com>
2025-09-19 09:15:53 +0800

    .

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  
 };
 
 export default config;
