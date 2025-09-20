87cf8a6714ab34e3922ac2d5741097f2ffe430fc
Zeddli <ruj8320@gmail.com>
2025-09-20 22:35:39 +0800

    main page for admin

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddNewLiveSession from '../../pages/AddNewLiveSession'
+
+/**
+ * Add New Live Session route page
+ * Admin interface accessible at /add-new-live-session
+ */
+export default function AddNewLiveSessionPage() {
+  return (
+    <AddNewLiveSession />
+  )
+} 
